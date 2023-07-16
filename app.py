56c71633c811b247f9695aeee5b1e63c8e77b11d
tnrzk13 <tnrzk13@gmail.com>
2023-07-16 00:37:29 -0400

    split pkl file for size limit

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from fastai.vision.all import *
 import gradio as gr
 
+
+def combine_files(file_path, number_of_parts):
+    data = b''
+    for i in range(number_of_parts + 1):
+        with open(f'{file_path}.part{i}', 'rb') as f:
+            data += f.read()
+    with open(file_path, 'wb') as f:
+        f.write(data)
+
+combine_files('chestXray.pkl', 5)
 learn = load_learner('chestXray.pkl')
 
 categories = ('No Pneumonia', 'Pneumonia')
